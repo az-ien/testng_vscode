25ad5ab299672ad22776c2714b021dae08a25474
az-ien <azlan.malik@iengineering.com>
2025-11-05 17:54:05 +0500

    first commit

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+         int a = 2;
+        int b = 3;
+        int result = a + b;
+
+
+        Assert.assertEquals(result, 5, "Sum should be 5");
+    }
+}
