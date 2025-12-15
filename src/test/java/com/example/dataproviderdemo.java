1f4023e7b68c7d8ee0c667c31326c3ae0061d169
az-ien <azlan.malik@iengineering.com>
2025-12-15 19:58:09 +0500

    moved dataprovide

--- src/test/java/com/example/dataproviderdemo.java
+++ src/test/java/com/example/dataproviderdemo.java
@@ -1,15 +1,14 @@
-package com.example.day45;
-
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
+package com.example;
 
 import java.time.Duration;
 
-import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 public class dataproviderdemo {
 
