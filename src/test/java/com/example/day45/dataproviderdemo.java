4fcffbda2682ddb408be396184996dd29d19553b
az-ien <azlan.malik@iengineering.com>
2025-12-15 16:12:25 +0500

    Wooohhhoooooo data provider done eeeeaaassssyyyy

--- src/test/java/com/example/day45/dataproviderdemo.java
+++ src/test/java/com/example/day45/dataproviderdemo.java
@@ -1,6 +1,7 @@
 package com.example.day45;
 
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 
 import java.time.Duration;
 
@@ -27,12 +28,12 @@ driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 
 
-@Test
-void testLogin()
+@Test(dataProvider="dp")  // dataprovider passed to the test with the name 
+void testLogin(String email, String pwd) // passing the parameters in the format for each of the data
 {
     driver.get("https://tutorialsninja.com/demo/index.php?route=account/login");
-    driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys("email");
-    driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys("pwd");
+    driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys(email);
+    driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys(pwd);
     driver.findElement(By.xpath("//input[@value='Login']")).click();
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
@@ -47,6 +48,63 @@ void tearDown()
 {
 driver.quit();
 }
+
+
+@DataProvider(name="dp", indices={3,3})  // some name given, indices will send the 4th email and password 
+Object[][] loginData()
+ {
+    // for excel make one loop and get data from excel data and return it to method only
+// excel is easily formatted data in excel but can be from other file types as well 
+// object is like json object so that there can be multiple data types returned not specified by any one datatype
+
+    Object data[][] = {   
+        {"abc@gmail.com","test123"},
+        {"xyz@gmail.com","test012"},
+        {"john@gmail.com","test@123"},
+       {"pavano123@gmail.com","test@123"},
+         {"johncanedy@gmail.com","test"},
+     };
+    
+     return data; //return is  String[][] so method should be String[][]
+
+ }
+
+
+
+
+// String[][] loginData()
+// {
+
+//     String data[][] = {   
+//         {"abc@gmail.com","test123"},
+//         {"xyz@gmail.com","test012"},
+//         {"john@gmail.com","test@123"},
+//         {"pavano123@gmail.com","test@123"},
+//         {"johncanedy@gmail.com","test"},
+//     };
+    
+//     return data; //return is  String[][] so method should be String[][]
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
